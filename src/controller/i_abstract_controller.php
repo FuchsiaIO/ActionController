85d4ee57ec68829ec77ff892af8ccdfed14f2dd3
FuchsiaIO <siras@live.com>
2015-11-05 14:02:21 -0500

    Fix PHP Fatal error:  Interfaces may not include member variables

--- src/controller/i_abstract_controller.php
+++ src/controller/i_abstract_controller.php
@@ -4,12 +4,8 @@ namespace ActionController\Controller;
 
 interface iAbstractController
 {
-  public $view;
-  protected $data;
-  protected $render;
-  
   public function __construct();
-  public function respondTo(callable $lambda);
+  public function respondTo( callable $lambda );
   public function getData();
   public function __set( $name, $value);
   public function __get( $name);
